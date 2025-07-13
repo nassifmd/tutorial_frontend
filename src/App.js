997d485a7e40a1f143d20cca8c6fdd06d2f80962
Nassifmd <nasnickmd@gmail.com>
2025-07-13 12:13:20 +0200

    add/

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css'; // Import your stylesheet
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5001/api/data')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div className="app-container">
+      <h1 className="app-title">Dummy Data</h1>
+      <div className="card-grid">
+        {data.map(item => (
+          <div key={item.id} className="card">
+            <h2 className="card-id">ID: {item.id}</h2>
+            <p className="card-name">Name: {item.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
